1682b2dab4423f6bf3617d67a1ed328732d53761
atuar123 <atuar.cse@gmail.com>
2021-09-10 20:26:40 +0600

    file created and updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import HeaderComponent from './components/layout/HeaderComponent';
+import FooterComponent from './components/layout/FooterComponent';
 
 function App() {
   return (
-    <div className="App">
-     <h2>Employee Management App</h2>
+    <div>
+      <HeaderComponent/>
+      <div className="container">
+          <h2>Employee Management App</h2>
+      </div>
+      <FooterComponent/>
     </div>
+    
   );
 }
 
---
+++ src/components/employee/CreateEmployee.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class CreateEmployee extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CreateEmployee;
